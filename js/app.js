6b49b12ab4652003bf7a6b99adbe2fcb5cbc5bba
shmelevsergei <shmelevsergei86@gmail.com>
2022-10-19 18:32:56 +0300

    all

--- js/app.js
+++ js/app.js
@@ -1,11 +1,32 @@
-const swiperPhoto = new Swiper('.slider-photo', {
+const swiperPhoto = new Swiper('.photo__slider', {
   navigation: {
-    nextEl: '.button-next',
-    prevEl: '.button-prev',
+    nextEl: '.photo__slider-btn--next',
+    prevEl: '.photo__slider-btn--prev',
   },
-  slidesPerView: 2,
+  slidesPerView: 2.38,
   spaceBetween: 30,
   loop: true,
+  breakpoints: {
+    // when window width is >= 320px
+    320: {
+      slidesPerView: 1.25,
+      spaceBetween: 10
+    },
+    // when window width is >= 480px
+    480: {
+      slidesPerView: 1.75,
+      spaceBetween: 10
+    },
+    // when window width is >= 640px
+    767: {
+      slidesPerView: 2,
+      spaceBetween: 20
+    },
+    1430: {
+      slidesPerView: 2.38,
+      spaceBetween: 30
+    }
+  }
 });
 
 const swiperNumber = new Swiper('.slider-number', {
@@ -18,6 +39,7 @@ const swiperNumber = new Swiper('.slider-number', {
     el: '.swiper-pagination',
   },
   loop: true,
+  
 });
 
 const swiperNumberFooter = new Swiper('.footer-number-slider', {
@@ -25,9 +47,39 @@ const swiperNumberFooter = new Swiper('.footer-number-slider', {
     nextEl: '.footer-number-button-next',
     prevEl: '.footer-number-button-prev',
   },
-  slidesPerView: 4,
-  spaceBetween: 30,
   loop: true,
+  breakpoints: {
+    // when window width is >= 320px
+    320: {
+      slidesPerView: 1.5,
+      spaceBetween: 10,
+      centeredSlides: true
+    },
+    // when window width is >= 480px
+    480: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+      centeredSlides: false
+    },
+    // when window width is >= 640px
+    767: {
+      slidesPerView: 2.5,
+      spaceBetween: 10
+    },
+    992: {
+      slidesPerView: 3.5,
+      spaceBetween: 20
+    },
+    1430: {
+      slidesPerView: 4,
+      spaceBetween: 20,
+      
+    },
+    1920: {
+      slidesPerView: 4,
+      spaceBetween: 30
+    }
+  }
 });
 
 const swiperEntertainment = new Swiper('.entertainment-slider', {
@@ -35,10 +87,27 @@ const swiperEntertainment = new Swiper('.entertainment-slider', {
     nextEl: '.entertainment-slide-button-next',
     prevEl: '.entertainment-slide-button-prev',
   },
-  slidesPerView: 2.1,
+  
   loop: true,
-  centeredSlides: true,
-  spaceBetween: 30,
+  breakpoints: {
+    // when window width is >= 320px
+    320: {
+      slidesPerView: 1.1,
+      spaceBetween: 10,
+      centeredSlides: true,
+    },
+    768: {
+      slidesPerView: 1.5,
+      spaceBetween: 20,
+      centeredSlides: false,
+    },
+
+    992: {
+      slidesPerView: 2.1,
+      centeredSlides: true,
+      spaceBetween: 30,
+    }
+  }
 });
 
 const swiperTerr = new Swiper('.terr-swiper', {
@@ -49,6 +118,46 @@ const swiperTerr = new Swiper('.terr-swiper', {
   slidesPerView: 1,
   loop: true,
   spaceBetween: 30,
+  breakpoints: {
+    // when window width is >= 320px
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 10,
+      pagination: {
+        el: '.swiper-pagination',
+      },
+    },
+  }
+});
+
+const swiperRestor = new Swiper('.restor-swiper', {
+  navigation: {
+    nextEl: '.restor-slider-button-next',
+    prevEl: '.restor-slider-button-prev',
+  },
+  loop: true,
+  
+  breakpoints: {
+    // when window width is >= 320px
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 10,
+    },
+
+    524: {
+      slidesPerView: 2,
+      spaceBetween: 10,
+    },
+
+    992: {
+      slidesPerView: 3,
+      spaceBetween: 30,
+    },
+
+    1430: {
+      slidesPerView: 4,
+    },
+  }
 });
 
 const swiperInclude = new Swiper('.include-slider', {
@@ -71,13 +180,58 @@ const swiperSpa = new Swiper('.spa-slider', {
   spaceBetween: 30,
 });
 
+const swiperImage = new Swiper('.image-swiper', {
+  navigation: {
+    nextEl: '.image-slider-button-next',
+    prevEl: '.image-slider-button-prev',
+  },
+  slidesPerView: 1,
+  loop: true,
+  spaceBetween: 10,
+  pagination: {
+    el: '.swiper-pagination',
+  },
+});
+
 const swiperSport = new Swiper('.sport-slider', {
   navigation: {
     nextEl: '.sport-slide-button-next',
     prevEl: '.sport-slide-button-prev',
   },
-  slidesPerView: 2.1,
   loop: true,
-  centeredSlides: true,
-  spaceBetween: 30,
+  breakpoints: {
+    // when window width is >= 320px
+    320: {
+      slidesPerView: 1.1,
+      spaceBetween: 10,
+      centeredSlides: true,
+    },
+    768: {
+      slidesPerView: 1.5,
+      spaceBetween: 20,
+      centeredSlides: false,
+    },
+
+    992: {
+      slidesPerView: 2.1,
+      centeredSlides: true,
+      spaceBetween: 30,
+    }
+  }
 });
+
+
+
+
+document.addEventListener('DOMContentLoaded', () => {
+  const menu = document.querySelector('.header__menu')
+  const buttonMenu = document.querySelector('.menu__burger')
+  const bodyMenu = document.querySelector('.body')
+  const menuMobile = document.querySelector('.menu__mobile')
+
+  buttonMenu.addEventListener('click', () => {
+    menu.classList.toggle('open'),
+    bodyMenu.classList.toggle('open'),
+    menuMobile.classList.toggle('active')
+  })
+})
